f9d3e0cec99a6dfa5540551b71e666bae4fe60fe
Lokesh Kumar V <lokeshkumarv2002@gmail.com>
2023-12-28 19:24:32 +0530

    firt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import HeaderTop from '@/components/HeaderTop'
+import HeaderMain from '@/components/HeaderMain'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <HeaderTop />
+        <HeaderMain />
+        <Navbar />
+        {children}</body>
     </html>
   )
 }
